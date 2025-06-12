7987455c74e2d74149aefa4ca9974e7b382059a9
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-06-12 15:00:08 +0600

    error handling

--- Module3/dist/app/app.js
+++ Module3/dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,6 +18,47 @@ const todos_routes_1 = require("./todos/todos.routes");
 // middleware
 app.use(express_1.default.json());
 app.use("/todos", todos_routes_1.todosRouter);
+app.get("/", (req, res, next) => {
+    console.log({
+        url: req.url,
+        method: req.method,
+        header: req.header,
+    });
+    next();
+}, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        //   console.log(something);
+        res.json({
+            message: "Welcome to the todos app.",
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.json({
+            message: "Welcome to the todos app.",
+        });
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Route not found.",
+    });
+});
+// global error always writing down in the file.
+app.use((error, req, res, next) => {
+    if (error) {
+        res.json({
+            message: "Something went wrong from global error handler",
+        });
+    }
+});
 // app.get("/todos", (req: Request, res: Response) => {
 //   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
 //   // res.send(data);
--- Module3/src/app/app.ts
+++ Module3/src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 import { todosRouter } from "./todos/todos.routes";
 
@@ -7,6 +7,52 @@ app.use(express.json());
 
 app.use("/todos", todosRouter);
 
+app.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log({
+      url: req.url,
+      method: req.method,
+      header: req.header,
+    });
+    next();
+  },
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //   console.log(something);
+      res.json({
+        message: "Welcome to the todos app.",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.json({
+      message: "Welcome to the todos app.",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    message: "Route not found.",
+  });
+});
+
+// global error always writing down in the file.
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    res.json({
+      message: "Something went wrong from global error handler",
+    });
+  }
+});
 // app.get("/todos", (req: Request, res: Response) => {
 //   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
 
